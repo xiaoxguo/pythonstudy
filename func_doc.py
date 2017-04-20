fc1d823da9cbd44861a08187a1a4561c7ecc2b5a
guojiacheng <guojiacheng@guojiacheng.example.com>
2017-04-20 19:43:28 +0800

    fix

--- func_doc.py
+++ func_doc.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python
-#encoding=utf8
+#encoding:utf-8
 #Filename:func_doc.py
 
 def printMax(x,y) :
-  '''Prints the maximum of two numbers.
+  '''Print the maximum of two numbers 
 
-试试中文能用吗?
 The two values must be integers.'''
-  x = int(x) #convert to integers ,if possible
-  y = int(y)
 
-  if x > y :
-    print x , 'is maximum'
-  elif y > x :
-    print y , 'is maximum'
-
-printMax(3,5) 
-print printMax.__doc__
+ 
